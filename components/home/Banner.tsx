5f44ef05836e947535ce4ec4a08ab7257896a1b9
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-10-23 21:03:55 +0600

    homepage hero and intro

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Image from "next/image";
+import homecover from "../../assets/cover.jpg";
 const Banner = () => {
   return (
-    <div className="h-[calc(100vh_-_200px)] w-full flex flex-col items-center justify-center ">
-      <p>contains images banner in a carosal</p>
+    <div className="max-h-[calc(100vh_-_200px)] flex flex-col items-center justify-center w-full ">
+      <Image src={homecover} className="object-cover w-full h-full" />
     </div>
   );
 };
--- components/home/IntroAbout.tsx
+++ components/home/IntroAbout.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import React from "react";
 import introaboutender from "../../assets/introaboutender.jpeg";
+import devider1 from "../../assets/deviderType1.png";
 const IntroAbout = () => {
   return (
-    <div className="w-full bg-[#fcf3ea] h-[calc(50vh)] ">
-      <div className="h-full max-w-7xl">
-        <p>IntroAbout beesz events and there tagline</p>
-        <div className="w-[300px] h-[94px]">
-          <Image
-            src={introaboutender}
-            alt="introaboutender"
-            className="items-center"
-          />
+    <div className="w-full bg-[#fcf3ea] h-[calc(40vh)] flex flex-col items-center justify-start mx-auto">
+      <div className="w-full h-full max-w-7xl">
+        <div className="relative w-full h-full ">
+          <div className="flex flex-col items-center justify-center w-full pt-24 h-2/3">
+            <p className="p-8 pt-0 text-4xl font-medium tracking-widest text-center font-great_vibes md:text-4xl lg:text-5xl">
+              Where Your Dreames Becomes Reality
+            </p>
+          </div>
+          <div className="flex flex-row items-center justify-center py-8 mx-auto max-w-7xl h-1/3">
+            <Image src={devider1} width={300} height={50} className="h-12" />
+          </div>
         </div>
       </div>
     </div>
